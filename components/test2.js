e91e6e6b12817e836569e8b1cf3b7d6662b8aed8
Ludwig Sannes <wouter.ludwig.sannes@accenture.com>
2019-05-07 15:02:27 +0200

    doner

--- components/test2.js
+++ components/test2.js
@@ -1,13 +1,12 @@
 'use strict';
 
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 // var axios = require('axios');
 // var request = require('request');
 
 module.exports = {
   metadata: () => ({
-    name: 'testEndpoint28',
-    properties: {},
+    name: 'testEndpoint30',
     supportedActions: []
   }),
   invoke: (conversation, done) => {
@@ -49,24 +48,44 @@ module.exports = {
   //     conversation.reply(parse);
   //     done();
   // });
-    
-    function xhrOnLoad(){
-      var data = JSON.parse(this.response);
+  function xhrOnLoad(){
+      // var testArray = [{"CREATED_BY": 1,"LAST_UPDATED_BY": null,"LAST_AUDIT_CODE": null,"EXPENSE_REPORT_ID": 1,"EXPENSE_REPORT_DATE": null,"CASH_EXPENSE_PAID_DATE": null,"CREATION_DATE": null,"PREPARER_ID": 1120575,"LAST_UPDATED_DATE": null,"EXPENSE_REPORT_TOTAL": null,"PERSON_ID": 1122507,"FINAL_APPROVAL_DATE": null,"PURPOSE": null,"EXPENSE_STATUS_CODE": null,"EXPENSE_STATUS_DATE": null,"AUDIT_RETURN_REASON_CODE": null}];
+      // var data = JSON.parse(this.responseText);
       // var data = '{"status": "OK","message": "Succesfull retrieve." "results": [   {        "EXPENSE_REPORT_ID": 1,          "PREPARER_ID": 1120575,          "PERSON_ID": 1122507,          "EXPENSE_REPORT_DATE": null,          "EXPENSE_REPORT_TOTAL": null,          "PURPOSE": null,          "EXPENSE_STATUS_CODE": null,          "EXPENSE_STATUS_DATE": null,          "LAST_AUDIT_CODE": null,          "AUDIT_RETURN_REASON_CODE": null,          "FINAL_APPROVAL_DATE": null,          "CASH_EXPENSE_PAID_DATE": null,          "CREATION_DATE": null,          "CREATED_BY": null,          "LAST_UPDATED_BY": null,          "LAST_UPDATED_DATE": null      }  ]}';
-      if(data == null){
-        console.log('empty');
-        conversation.reply(toString(data));
-      }else{
-        console.log("yeet");
-        conversation.reply(toString(data));
+      // var testObj = data.results;
+      // if(testObj == null){
+        // console.log('empty');
+        // conversation.reply("lik mn tepel");
+        // done();
+      // }else{
+        // console.log("yeet");
+        // conversation.reply("doner?");
+        // var test = JSON.stringify(testArray[0].CREATED_BY);
+        var data = this.response; 
+        var test = data.results[0].CREATED_BY;
+        var jTest = JSON.stringify(test);
+        conversation.reply(jTest);
+        done();
+        
+        // for (let i = 0; i < data.length; i++) {
+        //   const element = JSON.stringify(data[i]
+        //   conversation.reply(element);
+        //   done();
+        // }
+
+
+
+
+        // var test = JSON.stringify(testArray[0]);
+        // conversation.reply(test);
+        // done();
       }
-      done();
-    }
+    // }
 
     var xhr = new XMLHttpRequest();
     xhr.open('GET', url);
     // xhr.setRequestHeader('content-type', 'application/json');
-    xhr.addEventListener('response', xhrOnLoad);
+    xhr.addEventListener('load', xhrOnLoad);
     // xhr.onload = function(){
     //     var data = JSON.parse(this.response);
     //     if(data == null){
@@ -78,10 +97,7 @@ module.exports = {
     //         }
     //         done();
     //       }
-          xhr.onerror = function (e) {
-              alert(e);
-            }
-            xhr.send();
+    xhr.send();
             
           }
         };
